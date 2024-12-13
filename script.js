861683e14e3b2a77497e0fffef89918149d919fc
Danilo Almeida Milhome <damilhome@gmail.com>
2024-12-13 18:22:35 -0300

    atualizando pokemon info na tela

--- script.js
+++ script.js
@@ -14,6 +14,31 @@ const specialAttack = document.getElementById('special-attack');
 const specialDefense = document.getElementById('special-defense');
 const speed = document.getElementById('speed');
 
+const updateTypes = (data) => {
+    types.innerHTML = '';
+    data.types.forEach(typeObj => {
+        types.innerHTML += `
+            <p class="type ${typeObj.type.name}">${typeObj.type.name.toUpperCase()}</p>
+        `;
+    });
+};
+
+const showPokemonInfo = data => {
+    pokemonName.textContent = data.name.toUpperCase();
+    pokemonId.textContent = `#${data.id}`;
+    weight.textContent = `Weight: ${data.weight}`;
+    height.textContent = `Height: ${data.height}`;
+    pokemonImg.setAttribute('src', data.sprites.front_default)
+    pokemonImg.setAttribute('alt', `${data.name} image.`)
+    updateTypes(data);
+    hp.textContent = data.stats[0].base_stat;
+    attack.textContent = data.stats[1].base_stat;
+    defense.textContent = data.stats[2].base_stat;
+    specialAttack.textContent = data.stats[3].base_stat;
+    specialDefense.textContent = data.stats[4].base_stat;
+    speed.textContent = data.stats[5].base_stat;
+};
+
 const fetchData = async (url) => {
     try {
         const res = await fetch(url);
@@ -41,7 +66,7 @@ searchButton.addEventListener('click', async e => {
     if(!data) {
         alert('Pokémon not found');
     } else {
-
+        showPokemonInfo(data)
     }
 
 })
